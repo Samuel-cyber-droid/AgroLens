e0cd469c5fa1974b6f17e10ef0483c142740f066
Samuel-cyber-droid <samy21jul@gmail.com>
2026-04-13 19:11:13 -0600

    feat: Configuracion inicial de Django y creacion de modelos relacinales de inventario

--- core/settings.py
+++ core/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'inventario',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'core.urls'
--- inventario/admin.py
+++ inventario/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Categoria, Producto, MovimientoInventario
 
-# Registramos los modelos básicos
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(MovimientoInventario)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    # Columnas que se verán en la tabla principal
+    list_display = ('nombre_comercial', 'ingrediente_activo', 'presentacion', 'stock_actual')
+    # Filtros laterales
+    list_filter = ('categoria',)
+    # Barra de búsqueda para el OCR
+    search_fields = ('nombre_comercial', 'ingrediente_activo')
+    # El stock es de solo lectura (se altera con movimientos)
+    readonly_fields = ('stock_actual',)
+
+@admin.register(MovimientoInventario)
+class MovimientoInventarioAdmin(admin.ModelAdmin):
+    list_display = ('producto', 'tipo', 'cantidad', 'fecha_movimiento', 'usuario')
+    list_filter = ('tipo', 'fecha_movimiento')
+    search_fields = ('producto__nombre_comercial',)
+    readonly_fields = ('fecha_movimiento',)
